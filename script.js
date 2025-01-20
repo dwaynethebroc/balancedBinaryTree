28bde2b1d9626a25ca0a4cd5d36e93dca4b52032
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2025-01-20 13:11:54 +1100

    initial structure

---
+++ script.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(data, leftChild, rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
+}
+
+class Tree {
+    constructor(numberArray, rootNode){
+        this.numberArray = numberArray;
+        this.rootNode = rootNode;
+    }
+}
