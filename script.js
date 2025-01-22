c6e34b9107214fcf807f69b2b17aebd58325e612
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2025-01-22 19:32:29 +1100

    insertion working

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ class Tree {
     }
 }
 
+//Meta function to call all related functions in order to not mess up recursion in sort and binary tree functions
 function masterFunction(array){
     console.log(`Original array: ${array}`);
     console.log(`Merge sort started`);
@@ -22,13 +23,14 @@ function masterFunction(array){
     const duplicatesremovedArray = [ ...new Set(sortedArray) ]; //ES6 removes array
     console.log(`Duplicates removed: ${duplicatesremovedArray}`);
     console.log('buildTree function starting');
-    const myTree = buildTree(duplicatesremovedArray, 0, duplicatesremovedArray.length - 1);
+    const myTree = buildTree(duplicatesremovedArray, 0, duplicatesremovedArray.length - 1); //build tree function
     console.log('buildTree function finished')
-    prettyPrint(myTree);
+    prettyPrint(myTree); //print Binary tree
     console.log(`Return value of buildTree function: ${myTree.data}`);
-
+    return myTree;
 }
 
+//Make the binary tree function
 function buildTree(array, start, end) {
     //base case: if there is only one element, make it a node and have no children
     if (start > end) return null;
@@ -46,6 +48,57 @@ function buildTree(array, start, end) {
     return root;
 }
 
+//Insert value into tree function
+//Traverse tree to correct place
+//Insert value into tree and move over following values
+//when node has children or not
+function insert(root, value){
+    //base case
+    //if the node doesn't exist, return the new node
+    if(root === null){
+        return new Node(value);
+    }
+    //no duplicate values
+    if(root.data === value){
+        console.log(`Duplicate value exists in tree already. Value of: ${value}`)
+        return root;
+    }
+    //move to left child if value is lower
+    //move to right child if value is higher
+    //if there are no children, base case creates new node to be attached with value
+    if(value < root.data){
+        root.left = insert(root.left, value);
+    } else if(value > root.data){
+        root.right = insert(root.right, value);
+    }
+
+    return root;
+}
+
+function deleteItem(root, value){
+    //base case
+    //if the node doesn't exist, return
+    if(root === null){
+        return root;
+    }
+    //no duplicate values
+    if(root.data === value){
+        console.log(`Value found and deleted`);
+        return root;
+    }
+    //move to left child if value is lower
+    //move to right child if value is higher
+    //if there are no children, base case creates new node to be attached with value
+    if(value < root.data){
+        deleteItem(root.left, value);
+    } else if(value > root.data){
+        deleteItem(root.right, value);
+    }
+
+    return root;
+}
+
+//Sort functions
 function mergeSort(arr){
     const mid = Math.floor(arr.length / 2);
     const leftList = arr.slice(0, mid);
@@ -80,6 +133,7 @@ function merge(leftList, rightList){
     return sortedArray.concat(leftUnfinished).concat(rightUnfinished);
 }
 
+//Print out tree function
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
       return;
@@ -92,6 +146,11 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
       prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
 };
- 
+
+//Example function 
 const myArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-masterFunction(myArray);
+const myBinaryTree = masterFunction(myArray);
+insert(myBinaryTree, 20);
+insert(myBinaryTree, 500);
+insert(myBinaryTree, 42);
+
